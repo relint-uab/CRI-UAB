c3be4e7deb0b361645c447d8b94601869df7573d
alexandrubala94 <alexandrubala94@gmail.com>
2018-04-11 02:46:08 +0300

    Modified the menus for account types, created the faculties pages and created the links to the pages.

---
+++ app/src/main/java/com/example/alexa/centreforinternationalrelationsuab/faculties/ExactSciencesEngineering.java
@@ -0,0 +1,15 @@
+package com.example.alexa.centreforinternationalrelationsuab.faculties;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.alexa.centreforinternationalrelationsuab.R;
+
+public class ExactSciencesEngineering extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exact_sciences_engineering);
+    }
+}
