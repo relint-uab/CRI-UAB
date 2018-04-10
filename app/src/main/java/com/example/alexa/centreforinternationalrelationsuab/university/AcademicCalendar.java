82a464b5d6abf8a544beacabe37fa4aaddaf618d
alexandrubala94 <alexandrubala94@gmail.com>
2018-04-11 01:29:12 +0300

    Added Academic calendar and mini description for Rel. Int.

---
+++ app/src/main/java/com/example/alexa/centreforinternationalrelationsuab/university/AcademicCalendar.java
@@ -0,0 +1,15 @@
+package com.example.alexa.centreforinternationalrelationsuab.university;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.alexa.centreforinternationalrelationsuab.R;
+
+public class AcademicCalendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_academic_calendar);
+    }
+}
---
+++ app/src/main/java/com/example/alexa/centreforinternationalrelationsuab/university/RelIntDescription.java
@@ -0,0 +1,15 @@
+package com.example.alexa.centreforinternationalrelationsuab.university;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.alexa.centreforinternationalrelationsuab.R;
+
+public class RelIntDescription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rel_int_description);
+    }
+}
